23b14192088fa8e5bfd5b2910555385193aed21c
swakastudio <swakastudio.satu@gmail.com>
2026-03-11 22:46:46 +0700

    Update WeddingCover.tsx

--- app/(wedding)/firoh-arofi/components/WeddingCover.tsx
+++ app/(wedding)/firoh-arofi/components/WeddingCover.tsx
@@ -72,7 +72,7 @@ THE WEDDING OF
 
 <h1
 style={{ fontFamily: "ClassiqueScript" }}
-className="text-[110px] md:text-[120px] leading-[0.9] mb-3"
+className="text-[75px] md:text-[120px] leading-[0.9] mb-3"
 >
 
 <span>Firoh</span>
