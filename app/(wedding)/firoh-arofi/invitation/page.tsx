949851ef35908f4bc7b14d712376414f1adc6805
swakastudio <swakastudio.satu@gmail.com>
2026-03-13 16:01:41 +0700

    Update page.tsx

--- app/(wedding)/firoh-arofi/invitation/page.tsx
+++ app/(wedding)/firoh-arofi/invitation/page.tsx
@@ -49,7 +49,7 @@ return(
 
 {/* BACKGROUND */}
 
-<div className="fixed inset-0 -z-10 overflow-hidden">
+<div className="fixed inset-0 -z-0 overflow-hidden">
 
   {/* SKY */}
   <div className="absolute inset-0 bg-[#e9f3f8]" />
