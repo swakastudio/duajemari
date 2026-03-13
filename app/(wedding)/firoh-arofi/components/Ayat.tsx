9a0dc7719afe273e404553f440540a85597ced35
swakastudio <swakastudio.satu@gmail.com>
2026-03-13 17:09:02 +0700

    Update Ayat.tsx

--- app/(wedding)/firoh-arofi/components/Ayat.tsx
+++ app/(wedding)/firoh-arofi/components/Ayat.tsx
@@ -4,29 +4,27 @@ export default function Ayat(){
 
 return(
 
-<section className="fade-up relative px-6 py-28 overflow-hidden">
+<section className="fade-up px-6 py-28">
 
-
-{/* SOFT BACKGROUND */}
+{/* BACKGROUND BLOCK */}
 
 <div className="
-absolute inset-0
-bg-gradient-to-b
-from-[#E7F2F6]/80
-via-[#E7F2F6]/60
-to-transparent
-"/>
+relative
+bg-[#E7F2F6]
+py-24
+px-8
+">
 
-
-<div className="relative z-10 max-w-3xl mx-auto text-center">
+<div className="max-w-2xl mx-auto text-center">
 
 
 {/* AYAT */}
 
 <p className="
-text-[16px]
-md:text-[18px]
-leading-[2.1]
+text-[15px]
+md:text-[16px]
+leading-[2.2]
+tracking-[0.02em]
 text-neutral-700
 font-light
 ">
@@ -39,29 +37,12 @@ antara kamu rasa kasih dan sayang.
 </p>
 
 
-{/* DIVIDER */}
-
-<div className="flex items-center justify-center gap-6 my-10">
-
-<div className="w-20 h-[1px] bg-neutral-300/70"></div>
-
-<div className="
-w-2
-h-2
-rounded-full
-bg-neutral-400
-"/>
-
-<div className="w-20 h-[1px] bg-neutral-300/70"></div>
-
-</div>
-
-
 {/* SOURCE */}
 
 <p className="
-text-[16px]
-tracking-[0.28em]
+mt-10
+text-[11px]
+tracking-[0.35em]
 text-neutral-500
 ">
 
@@ -69,6 +50,22 @@ QS. AR-RUM 30 : 21
 
 </p>
 
+
+{/* ORNAMENT */}
+
+<img
+src="/firoh-arofi/frame-bar.svg"
+alt="divider"
+className="
+mx-auto
+mt-6
+w-[160px]
+opacity-80
+"
+/>
+
+
+</div>
 
 </div>
 
