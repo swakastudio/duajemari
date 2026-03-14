65b458e77bc6bc6ec06d4c9a4de1b3f191d13b69
swakastudio <swakastudio.satu@gmail.com>
2026-03-14 16:22:51 +0700

    Update MenghitungHari.tsx

--- app/(wedding)/firoh-arofi/components/MenghitungHari.tsx
+++ app/(wedding)/firoh-arofi/components/MenghitungHari.tsx
@@ -61,7 +61,7 @@ return(
     <img
 src="/firoh-arofi/flower-top-haribahagia.gif"
 alt="flower-bottom"
-className="absolute left-1/2 -translate-x-1/2 bottom-[-35px] w-full pointer-events-none z-30"
+className="absolute left-1/2 -translate-x-1/2 bottom-[-70px] w-full pointer-events-none z-30"
 />
 
 
