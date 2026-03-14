dbc6d3dffce43978240de8972f4e3c0ac6ad8b71
swakastudio <swakastudio.satu@gmail.com>
2026-03-14 15:54:10 +0700

    Update HariBahagia.tsx

--- app/(wedding)/firoh-arofi/components/HariBahagia.tsx
+++ app/(wedding)/firoh-arofi/components/HariBahagia.tsx
@@ -9,7 +9,7 @@ return(
   <img
 src="/firoh-arofi/flower-top-haribahagia.gif"
 alt="flower-bottom"
-className="absolute left-1/2 -translate-x-1/2 top-[20px] w-full pointer-events-none z-30"
+className="absolute left-1/2 -translate-x-1/2 top-[0px] w-full pointer-events-none z-30"
 />
 
 <div className="mx-auto max-w-4xl text-center">
