1959738d2888a084715c00a786acaaae28ff65fb
swakastudio <swakastudio.satu@gmail.com>
2026-03-15 18:03:47 +0700

    Update HariBahagia.tsx

--- app/(wedding)/firoh-arofi/components/HariBahagia.tsx
+++ app/(wedding)/firoh-arofi/components/HariBahagia.tsx
@@ -15,7 +15,7 @@ return(
       absolute
       left-1/2
       -translate-x-1/2
-top-[500px]
+top-[450px]
       w-[120%]
 max-w-[720px]
       pointer-events-none
@@ -30,7 +30,7 @@ max-w-[720px]
       absolute
       left-1/2
       -translate-x-1/2
-bottom-[-45px]
+bottom-[-10px]
       w-[120%]
 max-w-[1080px]
       pointer-events-none
