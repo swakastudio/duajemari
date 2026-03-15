964efe7b95785df1e7ae8ac08b169f8cc9176891
swakastudio <swakastudio.satu@gmail.com>
2026-03-15 18:12:55 +0700

    Update HariBahagia.tsx

--- app/(wedding)/firoh-arofi/components/HariBahagia.tsx
+++ app/(wedding)/firoh-arofi/components/HariBahagia.tsx
@@ -4,7 +4,7 @@ export default function HariBahagia(){
 
 return(
 
-<section className="fade-up relative px-6 py-20 overflow-hidden flex justify-center">
+<section className="fade-up relative px-6 py-20 overflow-hidden flex justify-center items-center">
 
 <div className="relative w-full max-w-[900px] text-center justify-center">
 
@@ -46,93 +46,6 @@ HARI BAHAGIA
 <div className="grid md:grid-cols-2 gap-14 place-items-center">
 
 
-{/* ================= AKAD ================= */}
-
-<div className="relative w-[360px] h-[392px] mx-auto">
-
-{/* FRAME */}
-
-<img
-src="/firoh-arofi/frame-hari-bahagia.png"
-alt="frame"
-className="absolute inset-0 w-full h-full object-contain pointer-events-none z-10"
-/>
-
-
-{/* GLASS CARD */}
-
-<div className="
-absolute
-top-[18px]
-bottom-[18px]
-left-[18px]
-right-[18px]
-bg-white/15
-backdrop-blur-xl
-rounded-[26px]
-border border-white/30
-shadow-[0_20px_70px_rgba(0,0,0,0.08)]
-flex
-flex-col
-items-center
-justify-center
-text-center
-px-6
-py-7
-">
-
-<p className="text-[14px] tracking-[0.08em] text-[#3f4d58]">
-AKAD NIKAH
-</p>
-
-
-<div className="flex items-center gap-4 my-4">
-
-<div className="w-10 h-[1px] bg-neutral-300"></div>
-
-<span className="text-neutral-400 text-xs">✦</span>
-
-<div className="w-10 h-[1px] bg-neutral-300"></div>
-
-</div>
-
-
-<h3 className="font-ltsip text-[38px] tracking-[0.06em] text-[#3f4d58]">
-Sabtu
-</h3>
-
-
-<p className="
-text-[12px]
-md:text-[16px]
-leading-[2.25]
-tracking-[0.02em]
-text-neutral-700
-font-light
-">
-04 April 2026
-</p>
-
-
-<p className="
-text-[12px]
-md:text-[16px]
-leading-[2.25]
-tracking-[0.02em]
-text-neutral-700
-font-light
-">
-08.00 WIB
-</p>
-
-
-</div>
-
-
-</div>
-
-
-
 {/* ================= RESEPSI ================= */}
 
 <div className="relative w-[360px] h-[392px] mx-auto">
@@ -172,7 +85,7 @@ pb-7
 {/* HEADER */}
 
 <p className="text-[14px] tracking-[0.08em] text-[#3f4d58] mt-2">
-RESEPSI
+AKAD NIKAH & RESEPSI 
 </p>
 
 
