ce21f4603892e931adecb5e8e0e86ed7b99d140b
swakastudio <swakastudio.satu@gmail.com>
2026-03-16 11:21:02 +0700

    Update page.tsx

--- app/(wedding)/firoh-arofi/invitation/page.tsx
+++ app/(wedding)/firoh-arofi/invitation/page.tsx
@@ -84,9 +84,9 @@ PAGE CONTENT
 
 {/* FOOTER */}
 
-<footer className="text-center py-16">
+<footer className="text-center py-16 mt-16">
 
-<p className="text-[12px] tracking-[0.18em] text-neutral-500">
+<p className="text-[12px] tracking-[0.18em] text-[#FEFEFE]">
 DIBUAT OLEH
 </p>
 
@@ -100,7 +100,7 @@ mt-3
 text-[15px]
 font-medium
 tracking-[0.05em]
-text-[#FF4D6D]
+text-[#FEFEFE]
 hover:underline
 "
 >
