996dbb03fd8c6cd89b4d2139e5aa9f780b735ae4
swakastudio <swakastudio.satu@gmail.com>
2026-03-24 21:18:58 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,21 +38,29 @@ export default function Navbar() {
     <>
       {loading && <PageLoader />}
 
-      <nav className="w-full flex items-center justify-between px-5 md:px-10 py-5 border-b border-neutral-200 bg-white text-black relative">
+     <nav className="w-full flex items-center justify-between px-5 md:px-10 py-5 
+backdrop-blur-md bg-white/70 border-b border-white/30 
+text-black fixed top-0 left-0 z-50">
 
         {/* LOGO */}
         <button
-          onClick={() => navigate("/")}
-          className={animate ? "bounce-up delay-1" : ""}
-        >
-          <Image
-            src="/logonavbar.svg"
-            alt="duajemari logo"
-            width={110}
-            height={50}
-            priority
-          />
-        </button>
+  className="md:hidden w-8 h-8 relative flex items-center justify-center"
+  onClick={() => setMenuOpen(!menuOpen)}
+>
+
+  <span className={`absolute h-[2px] w-6 bg-black transition-all duration-300 ease-in-out
+    ${menuOpen ? "rotate-45 translate-y-0" : "-translate-y-2"}
+  `} />
+
+  <span className={`absolute h-[2px] w-6 bg-black transition-all duration-300 ease-in-out
+    ${menuOpen ? "opacity-0" : "opacity-100"}
+  `} />
+
+  <span className={`absolute h-[2px] w-6 bg-black transition-all duration-300 ease-in-out
+    ${menuOpen ? "-rotate-45 translate-y-0" : "translate-y-2"}
+  `} />
+
+</button>
 
         {/* DESKTOP MENU */}
         <div className="hidden md:flex items-center gap-8 text-sm tracking-widest">
@@ -90,13 +98,19 @@ export default function Navbar() {
 
         {/* MOBILE MENU */}
         {menuOpen && (
-          <div className="absolute top-full left-0 w-full bg-white border-t border-neutral-200 flex flex-col items-center py-6 gap-6 text-sm tracking-widest z-50">
+          <div
+  className={`absolute top-full left-0 w-full backdrop-blur-xl bg-white/80 
+  border-t border-white/30 flex flex-col items-center gap-6 text-sm tracking-widest
+  overflow-hidden transition-all duration-500 ease-in-out
+  ${menuOpen ? "max-h-60 py-6 opacity-100" : "max-h-0 opacity-0"}
+  `}
+>
 
-            <button onClick={() => navigate("/")}>BERANDA</button>
-            <button onClick={() => navigate("/porto")}>PORTOFOLIO</button>
-            <button onClick={() => navigate("/product")}>PRODUK</button>
+  <button onClick={() => navigate("/")}>BERANDA</button>
+  <button onClick={() => navigate("/porto")}>PORTOFOLIO</button>
+  <button onClick={() => navigate("/product")}>PRODUK</button>
 
-          </div>
+</div>
         )}
 
       </nav>
