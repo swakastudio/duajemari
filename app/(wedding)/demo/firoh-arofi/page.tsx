72a45ee46524e0ce182fe5044bd715f430d3d047
swakastudio <swakastudio.satu@gmail.com>
2026-04-26 23:12:49 +0700

    Delete app/(wedding)/demo/firoh-arofi directory

--- app/(wedding)/demo/firoh-arofi/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getTemplate } from "@/app/(wedding)/_templates/engine";
-
-export default function Page() {
-  const { Template, data } = getTemplate("firoh-arofi");
-
-  return <Template data={data} />;
-}
