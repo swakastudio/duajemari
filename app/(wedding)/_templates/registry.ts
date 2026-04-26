22681c55ad2e57518881cd19b63a122210f2e465
swakastudio <swakastudio.satu@gmail.com>
2026-04-26 23:12:38 +0700

    Delete app/(wedding)/_templates directory

--- app/(wedding)/_templates/registry.ts
+++
@@ -1,5 +0,0 @@
-import FirohArofiTemplate from "../firoh-arofi/FirohArofiTemplate";
-
-export const TEMPLATE_REGISTRY = {
-  "firoh-arofi": FirohArofiTemplate,
-};
--- app/(wedding)/_templates/types.ts
+++
@@ -1,3 +0,0 @@
-export type WeddingTemplateProps = {
-  data: any;
-};
