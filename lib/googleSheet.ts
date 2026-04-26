d0720f65b6655908417217e51bf9d0faafc22856
swakastudio <swakastudio.satu@gmail.com>
2026-04-27 01:40:21 +0700

    Update googleSheet.ts

--- lib/googleSheet.ts
+++ lib/googleSheet.ts
@@ -1,7 +1,11 @@
-export async function getPortfolioData() {
+export async function getPortfolioData(): Promise<any[]> {
   try {
     const SHEET_ID = "1oixS66qVeyYamJl2eqFRJwXCg8T8UFOJkguDam95E5o"
-    const API_KEY = "AIzaSyAWBP6FjqWDrUx6zLs3pBB4T6IE1D97zJ8"
+    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+
+    if (!API_KEY) {
+      throw new Error("API KEY tidak ditemukan")
+    }
 
     const res = await fetch(
       `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1?key=${API_KEY}`,
@@ -12,13 +16,11 @@ export async function getPortfolioData() {
       throw new Error("Gagal fetch Google Sheets")
     }
 
-    const data: any = await res.json()
+    const data = await res.json()
 
-    console.log("RAW DATA:", data)
+    const rows = data.values?.slice(1) || []
 
-    const rows: any[] = data.values?.slice(1) || []
-
-    return rows.map((row) => ({
+    return rows.map((row: string[]) => ({
       slug: row[0] || "",
       namaPria: row[1] || "",
       namaWanita: row[2] || "",
@@ -30,6 +32,7 @@ export async function getPortfolioData() {
       image3: row[8] || "",
       linkUndangan: row[9] || "",
     }))
+
   } catch (error) {
     console.error("ERROR GOOGLE SHEET:", error)
     return []
