cb4dce9f508cddf24bc2aef43fb7c867e59911f3
swakastudio <swakastudio.satu@gmail.com>
2026-04-27 00:35:01 +0700

    Update RoyalFlowerTemplate.tsx

--- app/(wedding)/_templates/royal-flower/RoyalFlowerTemplate.tsx
+++ app/(wedding)/_templates/royal-flower/RoyalFlowerTemplate.tsx
@@ -4,22 +4,27 @@ import Ayat from "./components/Ayat"
 import BrideGroom from "./components/BrideGroom"
 import Closing from "./components/Closing"
 
+// TYPE DATA (biar aman & gak typo)
 type WeddingData = {
-  mempelaiPria: string
-  mempelaiWanita: string
-  ayat: string
-  textClosing: string
+  mempelaiPria?: string
+  mempelaiWanita?: string
+  ayat?: string
+  textClosing?: string
 }
 
 export default function RoyalFlowerTemplate({ data }: { data: WeddingData }) {
-  return (
-    <main>
 
+  return (
+    <main className="flex flex-col gap-16">
+
+      {/* MEMPELAI */}
       <BrideGroom data={data} />
 
-      <Ayat ayat={data.ayat} />
+      {/* AYAT */}
+      <Ayat ayat={data.ayat || ""} />
 
-      <Closing text={data.textClosing} />
+      {/* CLOSING */}
+      <Closing text={data.textClosing || ""} />
 
     </main>
   )
