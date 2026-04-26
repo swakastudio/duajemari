e84f6470c959a31ffc66874cf813da83f9b31087
swakastudio <swakastudio.satu@gmail.com>
2026-04-27 00:33:06 +0700

    Update RoyalFlowerTemplate.tsx

--- app/(wedding)/_templates/royal-flower/RoyalFlowerTemplate.tsx
+++ app/(wedding)/_templates/royal-flower/RoyalFlowerTemplate.tsx
@@ -4,7 +4,14 @@ import Ayat from "./components/Ayat"
 import BrideGroom from "./components/BrideGroom"
 import Closing from "./components/Closing"
 
-export default function RoyalFlowerTemplate({ data }: any) {
+type WeddingData = {
+  mempelaiPria: string
+  mempelaiWanita: string
+  ayat: string
+  textClosing: string
+}
+
+export default function RoyalFlowerTemplate({ data }: { data: WeddingData }) {
   return (
     <main>
 
