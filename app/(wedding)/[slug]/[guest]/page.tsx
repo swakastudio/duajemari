4b790ddd5d6a01b19e850c7d68245c1a5e35198a
swakastudio <swakastudio.satu@gmail.com>
2026-04-27 01:47:31 +0700

    Update page.tsx

--- app/(wedding)/[slug]/[guest]/page.tsx
+++ app/(wedding)/[slug]/[guest]/page.tsx
@@ -1,13 +1,20 @@
 import { getWeddingData } from "@/lib/getWeddingData"
 import RoyalFlowerTemplate from "@/app/(wedding)/_templates/royal-flower/RoyalFlowerTemplate"
 
-export default async function WeddingPage({ params }: any) {
+type Props = {
+  params: {
+    slug: string
+    guest: string
+  }
+}
+
+export default async function WeddingPage({ params }: Props) {
   const { slug, guest } = params
 
   const data = await getWeddingData(slug)
 
   if (!data) {
-    return <div>Undangan tidak ditemukan</div>
+    return <div>Undangan tidak ditemukan 😢</div>
   }
 
   return <RoyalFlowerTemplate data={data} guest={guest} />
