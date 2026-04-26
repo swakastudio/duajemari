35612d809b2fe2b5b7928580ffbc5af33126bbea
swakastudio <swakastudio.satu@gmail.com>
2026-04-27 00:55:04 +0700

    Update googleSheet.ts

--- lib/googleSheet.ts
+++ lib/googleSheet.ts
@@ -1,7 +1,7 @@
 export async function getPortfolioData() {
 
   const SHEET_ID = "1oixS66qVeyYamJl2eqFRJwXCg8T8UFOJkguDam95E5o"
-  const API_KEY = "AIzaSyAfh9L6JwIKgxFyc-UKQkPg7WEPpXuRpcQ"
+  const API_KEY = "AIzaSyAWBP6FjqWDrUx6zLs3pBB4T6IE1D97zJ8"
 
   const res = await fetch(
     `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1?key=${API_KEY}`,
